a63ab7923632b6204d2c96e4b8531f66f07c6488
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-08-13 15:24:55 -0600

    done with number buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,34 @@
 import React from "react";
 
 //import any components needed
+import NumberButton from "./NumberButton"
+
 
 //Import your array data to from the provided data file
+import {numbers} from "../../../data"
+
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
-  return (
+  
+  // const [numberState, setNumberState] = useState(numbers);
+   
+        return (
+          
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component */}
+       
+        {numbers.map(digit =>{
+          
+        return <NumberButton key={digit} number={digit}/> 
+          // {digit} </NumberButton>
+          })
+        }
+
     </div>
   );
 };
+
+export default Numbers;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {numbers} from "./data"
+// import {operators} from "./data"
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+console.log(numbers);
+// console.log(operators);
